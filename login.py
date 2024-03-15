014d66071083930df7de048feeefefc4dbb867ec
BigCucas <pedro.teles.martins.2006@gmail.com>
2024-03-15 17:51:20 +0000

    create login file

---
+++ login.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+# Connect to MySQL database
+connection = mysql.connector.connect(
+    host='localhost',
+    database='banking_system',
+    user='root',
+    password='Delete2006'
+)
+
+
